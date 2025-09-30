e7d45174f6a2212e37478b312dff845c6d0a3027
Hejdi Østergaard Nielsen <1000041081@mcdmstudent.dk>
2025-09-30 11:29:15 +0200

    bøh

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -5,8 +5,13 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <a href="https://youtu.be/dQw4w9WgXcQ?si=F8SxRFI2pWo_ENns" target="_blank" rel="noopener noreferrer" className={styles.link}>
-        <p className={styles.text}>© 2024 My Company</p>
+      <a
+        href="https://youtu.be/dQw4w9WgXcQ?si=F8SxRFI2pWo_ENns"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.link}
+      >
+        <p className={styles.text}>© 2024 My Company BØH</p>
       </a>
     </footer>
   );
